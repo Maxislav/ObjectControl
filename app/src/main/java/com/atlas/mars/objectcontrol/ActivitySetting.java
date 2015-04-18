7f5804123ce5fcd09ec95b2e9269877d5a0c5c37
mars <lmaxim@mail.ru>
2015-04-18 14:44:38 +0300

    localization Setting activity add rows

--- app/src/main/java/com/atlas/mars/objectcontrol/ActivitySetting.java
+++ app/src/main/java/com/atlas/mars/objectcontrol/ActivitySetting.java
@@ -21,8 +21,11 @@ public class ActivitySetting extends ActionBarActivity {
         init();
     }
     private void init(){
-       parsePasteWeb((WebView)findViewById(R.id.webMultipleSend), getString(R.string.multiple_send_help) );
-       parsePasteWeb((WebView)findViewById(R.id.confirmSend), getString(R.string.confirm_send_help) );
+
+        parsePasteWeb((WebView)findViewById(R.id.webMultipleSend), getString(R.string.multiple_send_help) );
+        parsePasteWeb((WebView)findViewById(R.id.confirmSend), getString(R.string.confirm_send_help) );
+       parsePasteWeb((WebView)findViewById(R.id.countHistory), getString(R.string.count_command_history) );
+       parsePasteWeb((WebView)findViewById(R.id.countHistoryInStart), getString(R.string.count_command_history_in_start) );
     }
     private void parsePasteWeb(WebView browser,String put ){
         CharSequence formatted = Phrase.from(web).put("content", put).format();
