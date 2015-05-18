18c21f43d8a8bb8fbe3f05e20f1a716064d780f9
mars <lmaxim@mail.ru>
2015-05-18 18:44:39 +0300

    start math geography  functions

---
+++ app/src/main/java/com/atlas/mars/objectcontrol/geoMath/GeoMath.java
@@ -0,0 +1,18 @@
+package com.atlas.mars.objectcontrol.geoMath;
+
+/**
+ * Created by mars on 5/18/15.
+ */
+public class GeoMath {
+    public float toAthimuth(float b1, float l1, float b2, float l2){
+
+        float b = (b1+b2)/2;
+        float db = b2- b1;
+        float dl = l2- l1;
+
+        Math.atan()
+
+        return 0.0f;
+
+    }
+}
