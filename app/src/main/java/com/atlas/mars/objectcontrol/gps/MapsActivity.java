5396b4d120fdbc51d63983a74bbbfd9bbc76c21c
Maxislav <gliderman2>
2015-06-14 19:41:58 +0300

    track test

--- app/src/main/java/com/atlas/mars/objectcontrol/gps/MapsActivity.java
+++ app/src/main/java/com/atlas/mars/objectcontrol/gps/MapsActivity.java
@@ -238,17 +238,17 @@ public class MapsActivity extends ActionBarActivity {
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.setType("*/*");
-//                intent.addCategory(Intent.CATEGORY_OPENABLE);
-//
-//                try {
-//                    startActivityForResult(
-//                            Intent.createChooser(intent, "Select a File to Upload"),1);
-//                } catch (android.content.ActivityNotFoundException ex) {
-//                    // Potentially direct the user to the Market with a Dialog
-//                    //Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
-//                }
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                try {
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select a File to Upload"),1);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    // Potentially direct the user to the Market with a Dialog
+                    //Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+                }
 
 
 
