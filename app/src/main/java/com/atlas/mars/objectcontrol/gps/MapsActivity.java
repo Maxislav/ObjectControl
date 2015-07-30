3ed554208c1e1d19627106ba2b8d541bfdbb7698
mars <lmaxim@mail.ru>
2015-07-30 18:32:13 +0300

    edit  onslider listener start

--- app/src/main/java/com/atlas/mars/objectcontrol/gps/MapsActivity.java
+++ app/src/main/java/com/atlas/mars/objectcontrol/gps/MapsActivity.java
@@ -172,7 +172,7 @@ public class MapsActivity extends ActionBarActivity implements FragmentZoomContr
         trackButton = new TrackButton(this, btnTrack, mMap);
         locationManagerGps = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManagerNet = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        new ScrollOnTouchListener(null, listContainer, this, null);
+       // new ScrollOnTouchListener(null, listContainer, this, null);
 
     }
 
