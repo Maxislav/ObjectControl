2e52b034198b1d617e7050bd1c5a40f637012d22
Maxim Lipatov <lmaxim@mail.ru>
2016-09-12 16:34:13 +0300

    olol

--- git-pass.js
+++ git-pass.js
@@ -11,7 +11,7 @@ fs.readFile('pass-config.json', (err, data) => {
 
   let pass = JSON.parse(data.toString()).pass
 
-  exec("cd app/; zip -P \""+pass+"\" build.gradle.zip -r build.gradle; git add *; git commit -m \"archive pass\"; git push ", (error, stdout, stderr) => {
+  exec("cd app/; zip -P \""+pass+"\" build.gradle.zip -r build.gradle; git add .; git commit -m \"archive pass\"; git push ", (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
